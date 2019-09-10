6f7a6f2279ab19e8d3fd0217b3fed250bc30fb3a
root <abhaysrivastava83@gmail.com>
2019-09-10 09:12:26 +0000

    added teraform files

---
+++ stack-abhay/terraform/example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  profile = "default"
+  region  = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-3ec9fd5b"
+  instance_type = "t2.micro"
+  key_name = "${var.key_name}"
+}
---
+++ stack-abhay/terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+default = ""
+  description = "Provide aws region"
+}
+variable "key_name" {
+default = ""
+}
+variable "vpc-id" {
+default = ""
+}
+variable "access_key" {
+default = ""
+}
+variable "secret_key" {
+default = ""
+}
